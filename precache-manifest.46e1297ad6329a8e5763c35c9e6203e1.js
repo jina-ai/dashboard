a093dbf494d1007500f0902d1574fa3cfdb1d2c6
Sergiy Chemodanov <>
2020-04-14 13:19:13 -0500

    Updates

---
+++ precache-manifest.46e1297ad6329a8e5763c35c9e6203e1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e903de813a7b7d15838099f0baf921",
+    "url": "/jina-ai.github.io/dashboard/index.html"
+  },
+  {
+    "revision": "9775a5cbe943e70824d0",
+    "url": "/jina-ai.github.io/dashboard/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "7ac1696fed3da0b69789",
+    "url": "/jina-ai.github.io/dashboard/static/css/main.8ee8c11f.chunk.css"
+  },
+  {
+    "revision": "9775a5cbe943e70824d0",
+    "url": "/jina-ai.github.io/dashboard/static/js/2.4c4891e5.chunk.js"
+  },
+  {
+    "revision": "469db3693394d5c6f9961612a2df00b8",
+    "url": "/jina-ai.github.io/dashboard/static/js/2.4c4891e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac1696fed3da0b69789",
+    "url": "/jina-ai.github.io/dashboard/static/js/main.12c6f914.chunk.js"
+  },
+  {
+    "revision": "9cfdaf20b47fe624233d",
+    "url": "/jina-ai.github.io/dashboard/static/js/runtime-main.4a3cd1c2.js"
+  },
+  {
+    "revision": "07aba578e23cf22dd1ce3fdc74aa0e9f",
+    "url": "/jina-ai.github.io/dashboard/static/media/jina-light.07aba578.svg"
+  }
+]);
