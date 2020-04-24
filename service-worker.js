56b9cbc1d4c506ac00f2886a84fcd87c012590c8
imsergiy <sergiychemodanov@gmail.com>
2020-04-24 13:44:24 +0000

    Deploying to gh-pages from  @ 4ca1653326d3f04c34e0e94d7053a14c660865f2 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.a636447661a7594530e232e5c1f848f3.js"
+  "/precache-manifest.271786596aa169fe4ce6e0b2909f5240.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
