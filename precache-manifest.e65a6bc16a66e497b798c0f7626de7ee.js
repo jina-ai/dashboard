cdc858799d609b0eb1167ad4aca6200c77033ab1
imsergiy <sergiychemodanov@gmail.com>
2020-04-25 14:52:27 +0000

    Deploying to gh-pages from  @ 3f34df7eca6af04117cf827e0b543714cb3da1b4 🚀

---
+++ precache-manifest.e65a6bc16a66e497b798c0f7626de7ee.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8725a7732c364af1ea131ca26c9d6aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "798e276e5d807aa46cf7",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "175ec97cf33f6805a91d",
+    "url": "/static/css/main.80df6919.chunk.css"
+  },
+  {
+    "revision": "798e276e5d807aa46cf7",
+    "url": "/static/js/2.16529566.chunk.js"
+  },
+  {
+    "revision": "469db3693394d5c6f9961612a2df00b8",
+    "url": "/static/js/2.16529566.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "175ec97cf33f6805a91d",
+    "url": "/static/js/main.52eeedf2.chunk.js"
+  },
+  {
+    "revision": "63128c4f5045ece8769d",
+    "url": "/static/js/runtime-main.ad3dc38c.js"
+  },
+  {
+    "revision": "07aba578e23cf22dd1ce3fdc74aa0e9f",
+    "url": "/static/media/jina-light.07aba578.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.271786596aa169fe4ce6e0b2909f5240.js"
+  "/precache-manifest.e65a6bc16a66e497b798c0f7626de7ee.js"
 );
 
 self.addEventListener('message', (event) => {
