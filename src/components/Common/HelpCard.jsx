8b3badc5d48da74be0920937bbf56a66f9679fec
Sergiy Chemodanov <sergiychemodanov@gmail.com>
2020-04-27 10:17:22 -0500

    add 404 page

---
+++ src/components/Common/HelpCard.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Card, Row, Col } from "react-bootstrap";
+
+class ChartNode extends React.Component {
+	render = () => {
+		const { title, content, icon, theme, link } = this.props;
+		return (
+			<a className="unstyled-link" href={link}>
+				<Card className="h-100">
+					<Card.Body className="pt-3">
+						<Row>
+							<Col xs="8" className="pr-0">
+								<h4>{title}</h4>
+								{content}
+							</Col>
+							<Col xs="4" className="pl-0">
+								<h1 className="mt-4 ml-4"><span className={`${icon} log-${theme}`} /></h1>
+							</Col>
+
+						</Row>
+					</Card.Body>
+
+				</Card>
+			</a>
+		)
+	}
+}
+
+export default ChartNode;
--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,16 +1,61 @@
 import React from "react";
-import { Container, Button } from "shards-react";
+import { Container, Row, Col } from "shards-react";
+import HelpCard from '../components/Common/HelpCard';
+import PageTitle from '../components/Common/PageTitle';
 
 const Errors = () => (
-  <Container fluid className="main-content-container px-4 pb-4">
-    <div className="error">
-      <div className="error__content">
-        <h2>404</h2>
-        <h3>This page does not exist yet!</h3>
-        <p>Try turning your clock forward.</p>
-        <Button pill>&larr; Go Back</Button>
-      </div>
-    </div>
+  <Container fluid className="main-content-container px-4">
+    <Row noGutters className="page-header py-4">
+      <PageTitle title="Resources" subtitle="Jina.ai" className="text-sm-left mb-3" />
+    </Row>
+    <Row>
+      <Col md="3" className="mb-4">
+        <HelpCard
+          title="Jina 101"
+          content="Understanding all key concepts of Jina in 3 minutes!"
+          link="https://101.jina.ai"
+          icon="fas fa-egg"
+          theme="warning"
+        />
+      </Col>
+      <Col md="3" className="mb-4">
+        <HelpCard
+          title="Jina Documentations"
+          content="The best way to learn Jina in depth."
+          link="https://docs.jina.ai"
+          icon="fas fa-book"
+          theme="primary"
+        />
+      </Col>
+      <Col md="3" className="mb-4">
+        <HelpCard
+          title='Jina "Hello World"'
+          content="A simple demo of image neural search for Fashion-MNIST."
+          link="https://github.com/jina-ai/jina#jina-hello-world-"
+          icon="fas fa-globe-europe"
+          theme="error"
+        />
+      </Col>
+      <Col md="3" className="mb-4">
+        <HelpCard
+          title='Visit us on Github!'
+          content="Explore more exciting open-source projects from us."
+          link="https://opensource.jina.ai"
+          icon="fab fa-github"
+          theme="info"
+        />
+      </Col>
+      <Col md="3" className="mb-4">
+        <HelpCard
+          title='We are hiring!'
+          content="Join us and build the next neural search eco system!"
+          link="https://www.linkedin.com/company/jinaai/jobs/"
+          icon="fab fa-linkedin"
+          theme="linkedin"
+        />
+      </Col>
+    </Row>
+
   </Container>
 );
 
