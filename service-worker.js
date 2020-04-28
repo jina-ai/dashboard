6528305b78eaeafeddd9d4c663d0a4520ea2206b
imsergiy <sergiychemodanov@gmail.com>
2020-04-28 14:02:55 +0000

    Deploying to gh-pages from  @ 547b70c2b0e84166c1366a3be2468bb24a1a4fd0 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.83fa4ec1160d2439f7a09f3befd3ec98.js"
+  "/precache-manifest.d3a64beedb246218c352994fc78cbaf0.js"
 );
 
 self.addEventListener('message', (event) => {
