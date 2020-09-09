b2aa7063dfd3d27530cc33e0d902b3ccd845d1bb
Pooja Maheshwari <pooja.maheshwari247@gmail.com>
2020-09-09 20:11:59 +0530

    Decompose Settings .jsx into small components (#34)
    refactor(Settings): decompose Settings .jsx into small components
    - break settings.jsx to use a composite element of label and form input
    - separate the form element details into base option and advance option list
    - change from class to hooks
    - commented out unused function `saveChanges`
    
    Co-authored-by: David Sanwald <DavidSanwald@users.noreply.github.com>

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,61 +1,106 @@
-import React from "react";
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Row,
-  Col,
-  Form,
-  FormInput,
-  Button,
-} from "shards-react";
+import React, { useState, useEffect } from "react";
+import { Card, CardHeader, CardBody, Row, Form } from "shards-react";
 import { Collapse } from "react-bootstrap";
 
-import { Store, Dispatcher, Constants } from "../../flux";
+import {
+  Store,
+  /* Dispatcher,
+  Constants, */
+} from "../../flux";
+import { baseOptions, advancedOptions } from "./options";
+import FormItem from "./FormItem";
 
-class SettingsCard extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      original: Store.getSettings(),
-      updates: {},
-      expanded: false,
-    };
-    Store.on("update-settings", this.getData);
+function SettingsCard() {
+  const [original, setOriginal] = useState(Store.getSettings());
+  const [updates, setUpdates] = useState({});
+  const [expanded, setExpanded] = useState(false);
+
+  function getData() {
+    const original = Store.getSettings();
+    setOriginal(original);
+    setUpdates({});
   }
 
-  componentWillUnmount = () => {
-    Store.removeListener("update-settings", this.getData);
-  };
+  useEffect(() => {
+    Store.on("update-settings", getData);
 
-  getData = () => {
-    const original = Store.getSettings();
-    this.setState({ original, updates: {} });
-  };
+    return function cleanup() {
+      Store.removeListener("update-settings", getData);
+    };
+  }, []);
 
-  updateSetting = (setting, value) => {
-    this.setState((prevState) => {
-      let { updates } = prevState;
-      updates[setting] = value;
-      return { updates };
-    });
-  };
+  function updateSetting(setting, value) {
+    const newUpdates = { ...updates };
+    newUpdates[setting] = value;
+    setUpdates(newUpdates);
+  }
 
-  saveChanges = () => {
-    const { original, updates } = this.state;
-
+  /** unused function 
+  function saveChanges(){
     const settings = { ...original, ...updates };
 
     Dispatcher.dispatch({
       actionType: Constants.SAVE_SETTINGS,
       payload: settings,
     });
-  };
+  }
+  */
 
-  toggleExpand = () => {
-    this.setState({ expanded: !this.state.expanded });
-  };
+  function toggleExpand() {
+    setExpanded(!expanded);
+  }
 
+
+  return (
+    <Card small className="mb-4">
+      <CardHeader className="border-bottom">
+        <h6 className="m-0">Connection Preferences</h6>
+      </CardHeader>
+      <CardBody>
+        <Form>
+          <Row form>
+            {baseOptions.map(({ label, placeholder, value }) => (
+              <FormItem
+                key={value}
+                label={label}
+                placeholder={placeholder}
+                value={value in updates ? updates[value] : original[value]}
+                onChange={(e) => updateSetting(value, e.target.value)}
+              />
+            ))}
+          </Row>
+          <strong
+            aria-controls="collapsed-form"
+            aria-expanded={expanded}
+            onClick={toggleExpand}
+            className="text-primary d-block mb-3 cursor-pointer"
+          >
+            Advanced{" "}
+            <i className="material-icons">
+              {expanded ? "arrow_drop_up" : "arrow_drop_down"}
+            </i>
+          </strong>
+          <Collapse in={expanded}>
+            <div id="collapsed-form">
+              <strong className="text-muted d-block mb-3">Endpoints</strong>
+              <Row form>
+                {advancedOptions.map(({ label, placeholder, value }) => (
+                  <FormItem
+                    key={value}
+                    label={label}
+                    placeholder={placeholder}
+                    value={value in updates ? updates[value] : original[value]}
+                    onChange={(e) => updateSetting(value, e.target.value)}
+                  />
+                ))}
+              </Row>
+            </div>
+          </Collapse>
+        </Form>
+      </CardBody>
+    </Card>
+  );
+=======
   render = () => {
     const { original, updates, expanded } = this.state;
     return (
@@ -173,6 +218,5 @@ class SettingsCard extends React.Component {
       </Card>
     );
   };
-}
 
 export default SettingsCard;
---
+++ src/components/Settings/options.js
@@ -0,0 +1,40 @@
+export const baseOptions = [
+  {
+    label: "Host",
+    placeholder: "0.0.0.0",
+    value: "host",
+  },
+  {
+    label: "Port",
+    placeholder: "5000",
+    value: "port",
+  },
+];
+
+export const advancedOptions = [
+  {
+    label: "Log",
+    placeholder: "/stream/log",
+    value: "log",
+  },
+  {
+    label: "Profile",
+    placeholder: "/stream/profile",
+    value: "profile",
+  },
+  {
+    label: "YAML",
+    placeholder: "/data/yaml",
+    value: "yaml",
+  },
+  {
+    label: "Shutdown",
+    placeholder: "/action/shutdown",
+    value: "shutdown",
+  },
+  {
+    label: "Ready",
+    placeholder: "/status/isready",
+    value: "ready",
+  },
+];
