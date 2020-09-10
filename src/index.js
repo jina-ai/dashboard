09b864afb36d50a4d90114619eb0a02ce144d6f9
David Sanwald <david.sanwald@gmail.com>
2020-09-10 10:38:51 +0200

    add color theme draft

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { ThemeProvider } from "emotion-theming";
+import { theme } from "./theme";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
