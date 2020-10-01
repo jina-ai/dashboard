92259465578c2a6ab7271eb31916560b63b9d803
Sergiy Chemodanov <sergiychemodanov@gmail.com>
2020-10-01 07:52:06 -0500

    refactor: flow selection

--- src/components/FlowChart/FlowSelection.jsx
+++ src/components/FlowChart/FlowSelection.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import { Dropdown } from "react-bootstrap";
 
+function SelectionIndicator({ selectedId, itemId }) {
+  return selectedId === itemId ? (
+    <i className="material-icons text-primary">radio_button_checked</i>
+  ) : (
+    <i className="material-icons">radio_button_unchecked</i>
+  );
+}
+
+function ConnectionIndicator({ connected, showIndicator }) {
+  if (!showIndicator) return null;
+  return connected ? (
+    <i className="material-icons ml-2 text-success">wifi</i>
+  ) : (
+    <i className="material-icons ml-2 text-warning">wifi_off</i>
+  );
+}
+
 export default function FlowSelection({
   flowOptions,
   loadFlow,
@@ -8,6 +25,14 @@ export default function FlowSelection({
   createNewFlow,
   connected,
 }) {
+  const userFlows = Object.entries(flowOptions).filter(
+    ([id, flow]) => flow.type !== "example"
+  );
+
+  const exampleFlows = Object.entries(flowOptions).filter(
+    ([id, flow]) => flow.type === "example"
+  );
+
   return (
     <Dropdown className="flow-selection">
       <Dropdown.Toggle>{flowOptions[selectedFlowId].name}</Dropdown.Toggle>
@@ -16,41 +41,23 @@ export default function FlowSelection({
           <i className="material-icons">add</i>New Flow
         </Dropdown.Item>
         <Dropdown.Header className="text-uppercase">Your Flows</Dropdown.Header>
-        {Object.entries(flowOptions)
-          .filter(([id, flow]) => flow.type !== "example")
-          .map(([flowId, flow], idx) => (
-            <Dropdown.Item onClick={() => loadFlow(flowId)} key={idx}>
-              {selectedFlowId === flowId ? (
-                <i className="material-icons text-primary">
-                  radio_button_checked
-                </i>
-              ) : (
-                <i className="material-icons">radio_button_unchecked</i>
-              )}
-              {flow.name}
-              {flow.type === "remote" &&
-                (connected ? (
-                  <i className="material-icons ml-2 text-success">wifi</i>
-                ) : (
-                  <i className="material-icons ml-2 text-warning">wifi_off</i>
-                ))}
-            </Dropdown.Item>
-          ))}
+        {userFlows.map(([flowId, flow], idx) => (
+          <Dropdown.Item onClick={() => loadFlow(flowId)} key={idx}>
+            <SelectionIndicator itemId={flowId} selectedId={selectedFlowId} />
+            {flow.name}
+            <ConnectionIndicator
+              showIndicator={flow.type === "remote"}
+              connected={connected}
+            />
+          </Dropdown.Item>
+        ))}
         <Dropdown.Header className="text-uppercase">Examples</Dropdown.Header>
-        {Object.entries(flowOptions)
-          .filter(([id, flow]) => flow.type === "example")
-          .map(([flowId, flow], idx) => (
-            <Dropdown.Item onClick={() => loadFlow(flowId)} key={idx}>
-              {selectedFlowId === flowId ? (
-                <i className="material-icons text-primary">
-                  radio_button_checked
-                </i>
-              ) : (
-                <i className="material-icons">radio_button_unchecked</i>
-              )}
-              {flow.name}
-            </Dropdown.Item>
-          ))}
+        {exampleFlows.map(([flowId, flow], idx) => (
+          <Dropdown.Item onClick={() => loadFlow(flowId)} key={idx}>
+            <SelectionIndicator itemId={flowId} selectedId={selectedFlowId} />
+            {flow.name}
+          </Dropdown.Item>
+        ))}
       </Dropdown.Menu>
     </Dropdown>
   );
