cbc08619326a089dbed572ee2c0b146aaed9cc0e
David Sanwald <DavidSanwald@users.noreply.github.com>
2020-10-01 11:13:17 +0200

    feat: add color theme

---
+++ src/theme.ts
@@ -0,0 +1,20 @@
+const PERSIAN_GREEN = "#009999";
+const GOLDEN_TANOI = "#FFCC66";
+const BITTER_SWEET = "#ff6666";
+const SCOOTER_GREEN = "#32C8CD";
+
+const theme = {
+  palette: {
+    background: { default: "#FFFFFF" },
+    primary: {
+      main: PERSIAN_GREEN,
+    },
+    secondary: { main: GOLDEN_TANOI },
+    warning: { main: GOLDEN_TANOI },
+    error: { main: BITTER_SWEET },
+    success: { main: PERSIAN_GREEN },
+    info: { main: SCOOTER_GREEN },
+  },
+};
+
+export { theme };
