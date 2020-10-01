10c35330eb1591de40130fe23c0cf53cdb75dcfe
David Sanwald <david.sanwald@gmail.com>
2020-10-01 13:37:40 +0200

    add first prototype layout for fallback page

--- src/FallbackPage.tsx
+++ src/FallbackPage.tsx
@@ -1,13 +1,33 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 import { FallbackProps } from "react-error-boundary";
+import PageTitle from "./components/Common/PageTitle";
+import { Button } from "shards-react";
 
 function FallbackPage({ error, resetErrorBoundary }: FallbackProps) {
   return (
-    <div role="alert">
-      <p>Something went wrong:</p>
-      <pre>{error!.message}</pre>
-      <button onClick={resetErrorBoundary}>Try again</button>
-    </div>
+    <Container fluid className="main-content-container p-5">
+      <Row noGutters className="page-header py-4">
+        <PageTitle
+          title="Error"
+          subtitle="This should not have happened"
+          className="text-sm-left my-3"
+        />
+      </Row>
+      <Row noGutters className="page-header py-4">
+        <Col xs="12" sm="3">
+          <p>Something went wrong:</p>
+          <pre>
+            <code className="text-warning">{error!.message}</code>
+          </pre>
+        </Col>
+      </Row>
+      <Row noGutters className="page-header">
+        <Col>
+          <Button onClick={resetErrorBoundary}>Try again</Button>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
