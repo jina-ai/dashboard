28514ed5b8f420e0602cfa90dadc6b9974fa19c7
David Sanwald <david.sanwald@gmail.com>
2020-10-01 13:47:02 +0200

    add back accidentally deleted named import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import { App } from "./App";
 import { ThemeProvider } from "emotion-theming";
 import { theme } from "./theme";
 
