33a2a2c10a5812bbe4d47b02a188c6999df31abf
Roshan Jossy <roshanjossey@gmail.com>
2021-01-07 22:48:55 +0100

    Use theme in PieChartBase, BarChartCard, SpeedCard

--- src/components/LogStream/BarChartBase.tsx
+++ src/components/LogStream/BarChartBase.tsx
@@ -6,40 +6,11 @@ import ChartElement, {
   ChartDataSets,
 } from "chart.js";
 import { LogLevelSummaryChartData } from "./types";
+import { getLevels } from "./levels";
 import { Theme, useTheme } from "@emotion/react";
 
 const DEFAULT_HEIGHT = 10;
 const DEFAULT_WIDTH = 100;
-
-const getLevels = (theme: Theme): { [key: string]: any } => {
-  const colorPalette = theme.palette;
-  return {
-    INFO: {
-      borderColor: colorPalette.info,
-      backgroundColor: "rgba(0, 153, 153, 0.9)",
-    },
-    SUCCESS: {
-      borderColor: colorPalette.success,
-      backgroundColor: "rgba(50, 200, 205, 0.9)",
-    },
-    WARNING: {
-      borderColor: colorPalette.warning,
-      backgroundColor: "rgba(255, 204, 102, 0.9)",
-    },
-    ERROR: {
-      borderColor: colorPalette.error,
-      backgroundColor: "rgba(255, 102, 102, 0.9)",
-    },
-    CRITICAL: {
-      borderColor: colorPalette.critical,
-      backgroundColor: "rgba(255, 70, 64, 0.9)",
-    },
-    DEBUG: {
-      borderColor: colorPalette.debug,
-      backgroundColor: "rgba(110, 114, 120, 0.9)",
-    },
-  };
-};
 
 function getParsedDatasets(data: LogLevelSummaryChartData, theme: Theme) {
   const levels = getLevels(theme);
