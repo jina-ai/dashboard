dc9547b57a348d1070d4a367697b50076f9400d2
BastinJafari <bastinjafari@gmail.com>
2021-01-14 10:30:27 +0100

    feat: add selector for Settings

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -2,9 +2,11 @@ import { combineReducers, createStore } from "redux";
 import flowReducer from "./flows/flows.reducer";
 import { FlowState } from "./flows/flows.types";
 import { composeWithDevTools } from "redux-devtools-extension";
+import { SettingsState } from "./settings/settings.types";
 
 export type State = {
   flowState: FlowState;
+  settingsState: SettingsState;
 };
 
 const rootReducer = combineReducers({
