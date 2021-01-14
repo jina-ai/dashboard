f70a140e033c4f899d67b6385d2196be7ec01759
BastinJafari <bastinjafari@gmail.com>
2021-01-14 10:09:27 +0100

    Feat: save settings in localStorage

--- src/redux/settings/settings.reducer.ts
+++ src/redux/settings/settings.reducer.ts
@@ -7,8 +7,17 @@ export default function settingsReducer(
 ): SettingsState {
   switch (action.type) {
     case SAVE_SETTINGS:
+      _saveSettingsInStore(action.payload.settings);
       return action.payload;
     default:
       return state;
   }
+}
+
+function _saveSettingsInStore(settings: any) {
+  Object.keys(settings).forEach((key) => {
+    localStorage.setItem(`preferences-${key}`, settings[key]);
+    const item = localStorage.getItem(`preferences-${key}`);
+    console.log(item);
+  });
 }
