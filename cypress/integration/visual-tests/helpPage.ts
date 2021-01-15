55b83c7cbe5e105ec636180b07a96c04c460bfca
Roshan Jossy <roshan.jossy@jina.ai>
2021-01-15 13:45:47 +0100

    test: add visual snapshot tests for help page

---
+++ cypress/integration/visual-tests/helpPage.ts
@@ -0,0 +1,16 @@
+const viewPortSizesToTest = [[1024, 768], 'macbook-11', 'macbook-13', 'macbook-15', 'macbook-16']
+
+describe('Help page', () => {
+  viewPortSizesToTest.forEach((size) => {
+    it(`checks snapshot of help page for size ${size}`, () => {
+      if (Cypress._.isArray(size)) {
+        cy.viewport(size[0], size[1])
+      } else {
+        cy.viewport(size)
+      }
+
+      cy.visit('/#/help')
+      cy.dataName('helpPage').matchImageSnapshot(`help-page-size-${size}`)
+    })
+  })
+})
