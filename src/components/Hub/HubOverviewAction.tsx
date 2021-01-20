ebe723f221ed496e0704ac0c1f4e3c1892087996
Roshan Jossy <roshan.jossy@jina.ai>
2021-01-20 11:27:32 +0100

    feat: style hub action items

--- src/components/Hub/HubOverviewAction.tsx
+++ src/components/Hub/HubOverviewAction.tsx
@@ -1,25 +1,52 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const ActionItemContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: space-between;
+  border-radius: 0.75rem;
+  padding: 1.5rem;
+  width: 35%;
+`;
+
+const ActionItemTitle = styled.span`
+  font-weight: 600;
+  font-size: 1.5rem;
+`;
+
+const ActionButton = styled.button`
+  background: linear-gradient(225deg, #00c9c9 0%, #009999 100%);
+  border-radius: 10px;
+  border: none;
+  padding: 1rem 1.5rem;
+  font-size: 1rem;
+  font-weight: 600;
+  color: #ffffff;
+`;
 
 type overviewActionType = {
-    title: string
-    description: string
-    buttonLabel: string
-}
+  title: string;
+  description: string;
+  buttonLabel: string;
+};
 
 type HubOverviewActionProps = {
-    overviewAction: overviewActionType
-}
+  overviewAction: overviewActionType;
+};
 
-const HubOverviewAction = ({overviewAction}: HubOverviewActionProps) => {
-    const {title, description, buttonLabel} = overviewAction
-    return (
-        <>
-            <h2>{title}</h2>
-            <p>{description}</p>
-            <button>{buttonLabel}</button>
-        </>
-    )
+const HubOverviewAction = ({ overviewAction }: HubOverviewActionProps) => {
+  const { title, description, buttonLabel } = overviewAction;
+  return (
+    <ActionItemContainer>
+      <>
+        <ActionItemTitle>{title}</ActionItemTitle>
+        <p>{description}</p>
+      </>
+      <ActionButton>{buttonLabel}</ActionButton>
+    </ActionItemContainer>
+  );
+};
 
-}
-
-export default HubOverviewAction
+export default HubOverviewAction;
--- src/views/HubView.tsx
+++ src/views/HubView.tsx
@@ -1,54 +1,10 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { Container, Row, Col } from "react-bootstrap";
-import { Dispatcher, Constants, Store } from "../flux";
+import React from "react";
+import { Container, Row } from "react-bootstrap";
 import { PageTitle } from "../components/Common/PageTitle";
-import { MultiFilterSelect } from "../components/Common/MultiFilterSelect";
-import { ExpandingSearchbar } from "../components/Common/ExpandingSearchbar";
-import ImageCard from "../components/Hub/ImageCard";
 import HubOverviewActionsContainer from "../components/Hub/HubOverviewActionsContainer";
-
-const categoryOptions = [
-  { value: "all", label: "All Categories" },
-  { value: "search", label: "Search" },
-  { value: "configurations", label: "Configurations" },
-];
-
-const sortOptions = [
-  { value: "suggested", label: "Suggested" },
-  { value: "highestRating", label: "Highest Rated" },
-  { value: "newest", label: "Newest" },
-];
+import HubImagesList from "../components/Hub/HubImagesList";
 
 const HubView = () => {
-  const [images, setImages] = useState([]);
-  const [sortType, setSortType] = useState("");
-  const [category, setCategory] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
-
-  useEffect(() => {
-    Store.on("update-hub", getHubImages);
-    // cleanup
-    return () => {
-      Store.removeListener("update-hub", getHubImages);
-    };
-  });
-
-  const search = useCallback(() => {
-    Dispatcher.dispatch({
-      actionType: Constants.SEARCH_HUB,
-      payload: { category, q: searchQuery, sort: sortType },
-    });
-  }, [sortType, category, searchQuery]);
-
-  useEffect(() => {
-    search();
-  }, [search]);
-
-  const getHubImages = () => {
-    const images = Store.getHubImages();
-    setImages(images);
-  };
-
   return (
     <Container fluid className="main-content-container px-0">
       <div className="px-4">
@@ -58,39 +14,7 @@ const HubView = () => {
         <Row>
           <HubOverviewActionsContainer />
         </Row>
-        <Row className="mb-4">
-          <Col md="8">
-            <MultiFilterSelect
-              options={categoryOptions}
-              onFilterChange={(option: any[]) => setCategory(option[0].value)}
-              className="hub-select mb-2 mr-0 mb-md-0 mr-md-2"
-              placeholder="All Categories"
-              isSearchable={false}
-            />
-            <MultiFilterSelect
-              options={sortOptions}
-              onFilterChange={(option: any[]) => setSortType(option[0].value)}
-              className="hub-select mb-2 mr-0 mb-md-0 mr-md-2"
-              placeholder="Suggested"
-              isSearchable={false}
-            />
-          </Col>
-          <Col md="4">
-            <ExpandingSearchbar
-              variant="gray"
-              placeholder="search hub..."
-              value={searchQuery}
-              onChange={setSearchQuery}
-            />
-          </Col>
-        </Row>
-        <Row>
-          {Object.keys(images).map((imageId) => (
-            <Col key={imageId} md="3" className="mb-4">
-              <ImageCard image={(images as any)[imageId]} />
-            </Col>
-          ))}
-        </Row>
+        <HubImagesList />
       </div>
     </Container>
   );
