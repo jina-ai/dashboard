0c6bd13954011ac1f261d34d05581eb3d852b9da
BastinJafari <bastinjafari@gmail.com>
2021-01-25 20:17:18 +0100

    Feat: New Flow Design

--- src/views/FlowView.tsx
+++ src/views/FlowView.tsx
@@ -238,6 +238,7 @@ export default function FlowView() {
                   config={{
                     readonly,
                     validateLink: validateLink,
+                    smartRouting: true,
                   }}
                 />
               </div>
