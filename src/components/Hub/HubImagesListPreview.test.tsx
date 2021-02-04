6b663544db66511a11fe0e9f8c2f6adbdafc9aff
Roshan Jossy <roshanjossey@gmail.com>
2021-02-05 00:42:20 +0100

    test: fix HubImagesListPreview test

--- src/components/Hub/HubImagesListPreview.test.tsx
+++ src/components/Hub/HubImagesListPreview.test.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "../../theme";
 import HubImagesListPreview from "./HubImagesListPreview";
 
 test("HubImagesListPreview", () => {
-  const component = renderer.create(<HubImagesListPreview />);
+  const component = renderer.create(
+    <ThemeProvider theme={theme}>
+      <HubImagesListPreview />
+    </ThemeProvider>
+  );
   let tree = component.toJSON();
   expect(tree).toMatchSnapshot();
 });
