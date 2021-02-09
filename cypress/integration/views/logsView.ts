e815fd0115e86d6f52396f034e5dc09cdc9f9a27
BastinJafari <bastinjafari@gmail.com>
2021-02-09 16:59:47 +0100

    Refactor: Increase timeout instead of timer

--- cypress/integration/views/logsView.ts
+++ cypress/integration/views/logsView.ts
@@ -1,44 +1,46 @@
-describe('Logs page', () => {
+describe("Logs page", () => {
   before(() => {
-    cy.visit('/')
-  })
+    cy.visit("/");
+  });
 
-  it('should redirect to /logs', () => {
-    cy.hash().should('eq', '#/logs')
-  })
+  it("should redirect to /logs", () => {
+    cy.hash().should("eq", "#/logs");
+  });
 
-  it('should display the title "Log Stream"', () => {
-    cy.dataName('title').should('have.text', 'Log Stream')
-  })
+  it("should display the title \"Log Stream\"", () => {
+    cy.dataName("title").should("have.text", "Log Stream");
+  });
 
-  describe('log level timeline', () => {
-    it('shows a graph with time axis and occurence count level', () => {
-      cy.dataName('logOccurenceTitle').should('contain.text', 'Log Occurences')
-      cy.dataName('logOccurencesChartLegend').should('contain.text', 'SUCCESS')
-    })
+  describe("log level timeline", () => {
+    it("shows a graph with time axis and occurence count level", () => {
+      cy.dataName("logOccurenceTitle").should("contain.text", "Log Occurences");
+      cy.dataName("logOccurencesChartLegend").should("contain.text", "SUCCESS");
+    });
 
-    context('on changing log duration', () => {
-      it('changes duration from the dropdown menu', () => {
-        cy.dataName('logOccurenceDurationSelectedOption').should('contain.text', '60 Seconds')
-        cy.dataName('logOccurenceDurationSelectedOption').click()
-        cy.dataName('logOccurenceDurationSelect').contains('15 Minutes').click()
-        cy.dataName('logOccurenceDurationSelectedOption').should('contain.text', '15 Minutes')
-      })
-    })
+    context("on changing log duration", () => {
+      it("changes duration from the dropdown menu", () => {
+        cy.dataName("logOccurenceDurationSelectedOption").should("contain.text", "60 Seconds");
+        cy.dataName("logOccurenceDurationSelectedOption").click();
+        cy.dataName("logOccurenceDurationSelect").contains("15 Minutes").click();
+        cy.dataName("logOccurenceDurationSelectedOption").should("contain.text", "15 Minutes");
+      });
+    });
 
-    context('when starting a flow',  () => {
-      it('should display the correct logging', () => {
-        cy.visit('#/flow')
-        cy.dataName('playButton').click()
-        cy.visit('#/logs')
-        cy.wait(5000)
-        cy.fixture('sample-output').then((output)=>{
-          console.log(output.logs[0])
-          output.logs.forEach((log, index) => {
-            cy.dataName(`logItem-${index}`).should('contain', log.message)
-          })
-        })
-      })
-    })
-  })
-})
+    context("when starting a flow", () => {
+      it("should display the correct logging",
+        {
+          defaultCommandTimeout: 10000,
+        }, () => {
+          cy.visit("#/flow");
+          cy.dataName("playButton").click();
+          cy.visit("#/logs");
+          cy.fixture("sample-output").then((output) => {
+            console.log(output.logs[0]);
+            output.logs.forEach((log, index) => {
+              cy.dataName(`logItem-${index}`).should("contain", log.message);
+            });
+          });
+        });
+    });
+  });
+});
