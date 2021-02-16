1fd160f54eaf5d803ed5ed9560d5a423bed8d708
BastinJafari <bastinjafari@gmail.com>
2021-02-16 13:18:36 +0100

    feat: immerjs global

--- src/redux/global/global.reducer.ts
+++ src/redux/global/global.reducer.ts
@@ -1,4 +1,3 @@
-import { GlobalActionTypes, GlobalState } from "./global.types";
 import {
   CLOSE_MODAL,
   HANDLE_CONNECTION_STATUS,
@@ -8,78 +7,49 @@ import {
   SHOW_ERROR,
   SHOW_MODAL,
   TOGGLE_SIDE_BAR,
-} from "./global.constants";
-import logger from "../../logger";
-import { HANDLE_NEW_LOG } from "../logStream/logStream.constants";
+} from "./global.constants"
+import logger from "../../logger"
+import { HANDLE_NEW_LOG } from "../logStream/logStream.constants"
+import produce from "immer"
 
-export default function globalReducer(
-  state: GlobalState = initialGlobalState,
-  action: GlobalActionTypes
-): GlobalState {
+const globalReducer = produce((draft, action) => {
   switch (action.type) {
     case HANDLE_CONNECTION_STATUS:
-      return _handleConnectionStatus(state, action.payload);
+      logger.log("handleLogConnectionStatus - status", action.payload.connected)
+      logger.log("handleLogConnectionStatus - message", action.payload.message)
+      draft.loading = false
+      draft.connected = action.payload.connected
+      break
     case TOGGLE_SIDE_BAR:
-      return {
-        ...state,
-        menuVisible: !state.menuVisible,
-      };
+      draft.menuVisible = !draft.menuVisible
+      break
     case SHOW_BANNER:
-      return {
-        ...state,
-        banner: {
-          message: action.payload.message,
-          theme: action.payload.theme,
-        },
-      };
+      draft.banner = {
+        message: action.payload.message,
+        theme: action.payload.theme,
+      }
+      break
     case HIDE_BANNER:
-      return {
-        ...state,
-        banner: null,
-      };
+      draft.banner = null
+      break
     case SHOW_ERROR:
-      return {
-        ...state,
-        banner: {
-          message: action.payload.message,
-          theme: "error",
-        },
-      };
+      draft.banner = {
+        message: action.payload.message,
+        theme: "error",
+      }
+      break
     case SHOW_MODAL:
-      return {
-        ...state,
-        modal: action.payload.modal,
-        modalParams: action.payload.modalParams,
-      };
+      draft.modal = action.payload.modal
+      draft.modalParams = action.payload.modalParams
+      break
     case CLOSE_MODAL:
-      return {
-        ...state,
-        modal: null,
-        modalParams: null,
-      };
-
+      draft.modal = null
+      draft.modalParams = null
+      break
     case HANDLE_NEW_LOG:
-      return {
-        ...state,
-        processes: {
-          ...state.processes,
-          [action.payload.process]: action.payload.name,
-        },
-      };
-    default:
-      return state;
+      draft.processes[action.payload.process] = action.payload.name
+      break
   }
-}
+}, initialGlobalState)
 
-function _handleConnectionStatus(
-  state: GlobalState,
-  { connected, message }: { connected: boolean; message: string }
-): GlobalState {
-  logger.log("handleLogConnectionStatus - status", connected);
-  logger.log("handleLogConnectionStatus - message", message);
-  return {
-    ...state,
-    loading: false,
-    connected: connected,
-  };
-}
+export default globalReducer
