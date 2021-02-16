a3f61a0cef8f9789b1f0e07eabd2d3a1a9d11999
BastinJafari <bastinjafari@gmail.com>
2021-02-16 12:31:08 +0100

    Fix: readonly bug

--- src/views/FlowView.tsx
+++ src/views/FlowView.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, useCallback } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React, { useEffect, useState, useCallback } from "react"
+import { useSelector, useDispatch } from "react-redux"
 import {
   selectFlowArguments,
   selectFlowChart,
@@ -7,8 +7,8 @@ import {
   selectRerender,
   selectSelectedFlowId,
   selectTooltipConfig,
-} from "../redux/flows/flows.selectors";
-import * as actions from "@bastinjafari/react-flow-chart-with-tooltips-and-multi-select/src/container/actions";
+} from "../redux/flows/flows.selectors"
+import * as actions from "@bastinjafari/react-flow-chart-with-tooltips-and-multi-select/src/container/actions"
 import {
   createNewFlow,
   deleteFlow,
@@ -17,81 +17,82 @@ import {
   startFlow,
   stopFlow,
   updateFlow,
-} from "../redux/flows/flows.actions";
-import html2canvas from "html2canvas";
-import { cloneDeep } from "lodash";
-import { copyToClipboard, formatAsYAML } from "../helpers";
-import { Card, Container, Row } from "shards-react";
-import { PageTitle } from "../components/Common/PageTitle";
-import FlowSelection from "../components/FlowChart/FlowSelection";
-import CommandBar from "../components/FlowChart/CommandBar";
+} from "../redux/flows/flows.actions"
+import html2canvas from "html2canvas"
+import { cloneDeep } from "lodash"
+import { copyToClipboard, formatAsYAML } from "../helpers"
+import { Card, Container, Row } from "shards-react"
+import { PageTitle } from "../components/Common/PageTitle"
+import FlowSelection from "../components/FlowChart/FlowSelection"
+import CommandBar from "../components/FlowChart/CommandBar"
 import {
   FlowChart,
   IChart,
   IOnLinkCompleteInput,
-} from "@bastinjafari/react-flow-chart-with-tooltips-and-multi-select";
-import Tooltip from "../components/FlowChart/Tooltip";
-import CustomNode from "../components/FlowChart/ChartNode";
-import CustomPort from "../components/FlowChart/NodePort";
-import Sidebar from "../components/FlowChart/Sidebar";
-import { showModal } from "../redux/global/global.actions";
-import { selectConnectionStatus } from "../redux/global/global.selectors";
+} from "@bastinjafari/react-flow-chart-with-tooltips-and-multi-select"
+import Tooltip from "../components/FlowChart/Tooltip"
+import CustomNode from "../components/FlowChart/ChartNode"
+import CustomPort from "../components/FlowChart/NodePort"
+import Sidebar from "../components/FlowChart/Sidebar"
+import { showModal } from "../redux/global/global.actions"
+import { selectConnectionStatus } from "../redux/global/global.selectors"
 
-import styled from "@emotion/styled";
-import logger from "../logger";
+import styled from "@emotion/styled"
+import logger from "../logger"
 
 const FlowViewContainer = styled.div`
   display: flex;
-`;
+`
 
 const FlowContainer = styled.div`
   overflow: hidden;
-`;
+`
 
 export default function FlowView() {
-  const dispatch = useDispatch();
-  useState(useSelector(selectRerender));
-  const connected = useSelector(selectConnectionStatus);
-  const selectedFlowId = useSelector(selectSelectedFlowId);
-  const flows = useSelector(selectFlows);
-  const flowArguments = useSelector(selectFlowArguments);
-  const flowChart = useSelector(selectFlowChart);
-  const { flow: chart, type: flowType } = flowChart;
+  const dispatch = useDispatch()
+  useState(useSelector(selectRerender))
+  const connected = useSelector(selectConnectionStatus)
+  const selectedFlowId = useSelector(selectSelectedFlowId)
+  const flows = useSelector(selectFlows)
+  const flowArguments = useSelector(selectFlowArguments)
+  const flowChart = useSelector(selectFlowChart)
+  const { flow: chart, type: flowType } = flowChart
   const chartWithTooltips = {
     ...chart,
     ...useSelector(selectTooltipConfig),
-  };
-  const [showOverlay, setShowOverlay] = useState<boolean>(false);
+  }
+  const [showOverlay, setShowOverlay] = useState<boolean>(false)
 
   const actionCallbacks = Object.keys(actions).reduce((obj: any, key: any) => {
     obj[key] = (...args: any) => {
-      let action = (actions as any)[key];
-      let newChartTransformer = action(...args);
-      let newChart = newChartTransformer(chart);
-      dispatch(updateFlow({ ...chartWithTooltips, ...newChart }));
-      return newChart;
-    };
-    return obj;
-  }, {});
+      let action = (actions as any)[key]
+      let newChartTransformer = action(...args)
+      const chartCopy = cloneDeep(chart) //todo this is necessary because our state management is not consistent. Remove with new flow library
+      let newChart = newChartTransformer(chartCopy)
+      dispatch(updateFlow({ ...chartWithTooltips, ...newChart }))
+      return newChart
+    }
+    return obj
+  }, {})
 
   useEffect(() => {
-    const chartContainer = document.querySelector(".chart-container");
+    const chartContainer = document.querySelector(".chart-container")
     if (chartContainer)
-      chartContainer.addEventListener("contextmenu", (e) => e.preventDefault());
-  }, []);
+      chartContainer.addEventListener("contextmenu", (e) => e.preventDefault())
+  }, [])
 
   const showCaptureOverlay = (showOverlay = true) => {
-    setShowOverlay(showOverlay);
-  };
+    setShowOverlay(showOverlay)
+  }
 
   const exportImage = (extension = "png") => {
-    const chartContainer = document.querySelector(".chart-container");
-    const captureOverlay = document.querySelector(".capture-overlay");
-    if (!chartContainer) return;
-    if (captureOverlay) captureOverlay.classList.add("fade-out");
+    const chartContainer = document.querySelector(".chart-container")
+    const captureOverlay = document.querySelector(".capture-overlay")
+    if (!chartContainer) return
+    if (captureOverlay) captureOverlay.classList.add("fade-out")
 
-    showCaptureOverlay();
-    setTimeout(() => showCaptureOverlay(false), 500);
+    showCaptureOverlay()
+    setTimeout(() => showCaptureOverlay(false), 500)
 
     let canvasParams = {
       foreignObjectRendering: true,
@@ -101,62 +102,62 @@ export default function FlowView() {
       scrollX: 0,
       scrollY: 0,
       scale: 5,
-    };
+    }
 
     html2canvas(chartContainer as HTMLElement, canvasParams).then((canvas) => {
-      var image = canvas.toDataURL(`image/${extension}`);
-      var link = document.getElementById("download-link");
-      if (!link) return;
-      link.setAttribute("download", `jina-flow-visual.${extension}`);
-      link.setAttribute("href", image);
-      link.click();
-    });
-  };
+      var image = canvas.toDataURL(`image/${extension}`)
+      var link = document.getElementById("download-link")
+      if (!link) return
+      link.setAttribute("download", `jina-flow-visual.${extension}`)
+      link.setAttribute("href", image)
+      link.click()
+    })
+  }
 
   const updateNode = (node: any) => {
-    let newChart = cloneDeep(chart);
-    newChart.nodes[node.id].label = node.label;
+    let newChart = cloneDeep(chart)
+    newChart.nodes[node.id].label = node.label
 
     let props = {
       ...node.properties,
       ...node.newProperties,
-    };
+    }
 
     Object.keys(props).forEach((id) => {
       if (props[id] === "" || typeof props[id] === "undefined") {
-        delete props[id];
+        delete props[id]
       }
-    });
+    })
 
-    newChart.nodes[node.id].properties = props;
-    dispatch(updateFlow({ ...chart, ...newChart }));
+    newChart.nodes[node.id].properties = props
+    dispatch(updateFlow({ ...chart, ...newChart }))
 
-    return newChart.nodes[node.id];
-  };
+    return newChart.nodes[node.id]
+  }
 
   const updateLink = (
     linkId: string,
     fromId: string,
     toId: string | undefined
   ) => {
-    if (fromId === toId) return;
-    let newChart = cloneDeep(chart);
+    if (fromId === toId) return
+    let newChart = cloneDeep(chart)
 
-    newChart.links[linkId].from.nodeId = fromId;
-    newChart.links[linkId].to.nodeId = toId;
+    newChart.links[linkId].from.nodeId = fromId
+    newChart.links[linkId].to.nodeId = toId
 
-    dispatch(updateFlow({ ...chart, ...newChart }));
-  };
+    dispatch(updateFlow({ ...chart, ...newChart }))
+  }
 
   const deleteSelection = () => {
-    actionCallbacks.onDeleteKey({});
-  };
+    actionCallbacks.onDeleteKey({})
+  }
 
   const copyChartAsYAML = useCallback(() => {
-    logger.log("copyChartAsYAML | chart:", chart);
-    copyToClipboard(formatAsYAML(chart, flowArguments));
-    alert("Chart copied to clipboard as YAML");
-  }, [chart, flowArguments]);
+    logger.log("copyChartAsYAML | chart:", chart)
+    copyToClipboard(formatAsYAML(chart, flowArguments))
+    alert("Chart copied to clipboard as YAML")
+  }, [chart, flowArguments])
 
   const validateLink = ({
     fromNodeId,
@@ -164,45 +165,45 @@ export default function FlowView() {
     fromPortId,
     toPortId,
   }: IOnLinkCompleteInput) => {
-    return !(fromNodeId === toNodeId || fromPortId === toPortId);
-  };
+    return !(fromNodeId === toNodeId || fromPortId === toPortId)
+  }
 
   const showImportModal = () => {
-    dispatch(showModal("import"));
-  };
+    dispatch(showModal("import"))
+  }
 
   const showNewFlowModal = () => {
-    dispatch(showModal("newFlow"));
-  };
+    dispatch(showModal("newFlow"))
+  }
 
   const handleCreateNewFlow = (e: any) => {
-    e.preventDefault();
-    e.stopPropagation();
+    e.preventDefault()
+    e.stopPropagation()
 
-    dispatch(createNewFlow());
-  };
+    dispatch(createNewFlow())
+  }
 
   const handleDeleteFlow = (e: any, flowId: any) => {
-    e.preventDefault();
-    e.stopPropagation();
+    e.preventDefault()
+    e.stopPropagation()
 
-    dispatch(deleteFlow(flowId));
-  };
+    dispatch(deleteFlow(flowId))
+  }
 
   const handleDuplicateFlow = () => {
-    const flowYAML = formatAsYAML(chart, flowArguments);
-    dispatch(duplicateFlow(flowYAML));
-  };
+    const flowYAML = formatAsYAML(chart, flowArguments)
+    dispatch(duplicateFlow(flowYAML))
+  }
 
   const handleStartFlow = () => {
-    dispatch(startFlow(selectedFlowId));
-  };
+    dispatch(startFlow(selectedFlowId))
+  }
 
   const handleStopFlow = () => {
-    dispatch(stopFlow(selectedFlowId));
-  };
+    dispatch(stopFlow(selectedFlowId))
+  }
 
-  const readonly = flowType !== "user-generated";
+  const readonly = flowType !== "user-generated"
 
   return (
     <Container fluid className="main-content-container px-0">
@@ -273,5 +274,5 @@ export default function FlowView() {
         </FlowViewContainer>
       </div>
     </Container>
-  );
+  )
 }
