a6df651abb83b83bc7a5aed1c4fc8274820731eb
BastinJafari <bastinjafari@gmail.com>
2021-02-25 13:48:53 +0100

    Style: Pods

--- src/components/FlowChart/NodePort.tsx
+++ src/components/FlowChart/NodePort.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React from "react"
+import styled from "@emotion/styled"
 
-export default function NodePort() {
+export function NodePortTop() {
   const NodePortOuter = styled.div`
+    position: relative;
+    right: 8px;
+    bottom: 5px;
     background-color: white;
     width: 1rem;
     height: 1rem;
@@ -11,8 +14,7 @@ export default function NodePort() {
     display: flex;
     justify-content: center;
     align-items: center;
-    margin: 0.3rem;
-  `;
+  `
 
   const NodePortInner = styled.div`
     background-color: #099;
@@ -20,10 +22,39 @@ export default function NodePort() {
     border-radius: 0.25rem;
     width: 0.5rem;
     height: 0.5rem; ;
-  `;
+  `
   return (
     <NodePortOuter>
       <NodePortInner />
     </NodePortOuter>
-  );
+  )
+}
+
+export function NodePortBottom() {
+  const NodePortOuter = styled.div`
+    position: relative;
+    right: 8px;
+    bottom: 8px;
+    background-color: white;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 0.5rem;
+    border: 1px solid rgba(0, 153, 153, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  `
+
+  const NodePortInner = styled.div`
+    background-color: #099;
+    color: white;
+    border-radius: 0.25rem;
+    width: 0.5rem;
+    height: 0.5rem; ;
+  `
+  return (
+    <NodePortOuter>
+      <NodePortInner />
+    </NodePortOuter>
+  )
 }
