740087185491c7b41774b134f828984a25e89042
BastinJafari <bastinjafari@gmail.com>
2021-03-02 18:14:21 +0100

    Refactor: remove more any

--- src/logger.ts
+++ src/logger.ts
@@ -1,58 +1,63 @@
-import { saveAs } from "file-saver";
+import { saveAs } from "file-saver"
 
-function handleErrorMessage(msg: any, url: any, line: any) {
+function handleErrorMessage(msg: string, url: string, line: string) {
   // @ts-ignore
-  logger.log("window.onerror - ERROR", msg, url, `line: ${line}`);
+  logger.log("window.onerror - ERROR", msg, url, `line: ${line}`)
 }
 
 function pushLog(data: any) {
-  (window as any).logs.push(data);
+  ;(window as any).logs.push(data)
 }
 
 function clearLogs() {
-  (window as any).logs = [];
+  ;(window as any).logs = []
 }
 
 const logger = {
   log: function (...arg: any) {
-    if (!(window as any).logsEnabled) return;
-    let args = [...(arguments as any)];
-    console.log(...args);
-    pushLog(args);
+    if (!(window as any).logsEnabled) return
+    let args = [...(arguments as any)]
+    console.log(...args)
+    pushLog(args)
   },
   isEnabled: function () {
-    return (window as any).logsEnabled;
+    return (window as any).logsEnabled
   },
   enable: function () {
-    const _navigator: any = {};
-    for (let i in window.navigator)
-      _navigator[i] = (window as any).navigator[i];
-    clearLogs();
-    pushLog(_navigator);
-    window.addEventListener("error", handleErrorMessage as any);
-    (window as any).logsEnabled = true;
+    const _navigator: any = {}
+    for (let i in window.navigator) _navigator[i] = (window as any).navigator[i]
+    clearLogs()
+    pushLog(_navigator)
+    window.addEventListener(
+      "error",
+      (handleErrorMessage as unknown) as EventListenerOrEventListenerObject
+    )
+    ;(window as any).logsEnabled = true
   },
   disable: function () {
-    window.removeEventListener("error", handleErrorMessage as any);
-    (window as any).logsEnabled = false;
+    window.removeEventListener(
+      "error",
+      (handleErrorMessage as unknown) as EventListenerOrEventListenerObject
+    )
+    ;(window as any).logsEnabled = false
   },
   setFormat: function (format = "json") {
-    (window as any).logsFormat = format;
+    ;(window as any).logsFormat = format
   },
   exportLogs: function () {
-    const format = (window as any).logsFormat || "json";
-    const logs = (window as any).logs;
-    let content = "[\n";
+    const format = (window as any).logsFormat || "json"
+    const logs = (window as any).logs
+    let content = "[\n"
     for (let i = 0; i < logs.length; ++i) {
-      let args = logs[i];
-      content += JSON.stringify(args) + `${i < logs.length - 1 ? "," : ""}\n`;
+      let args = logs[i]
+      content += JSON.stringify(args) + `${i < logs.length - 1 ? "," : ""}\n`
     }
-    content += "]";
+    content += "]"
 
-    let filename = `jina-dashboard-debug-output-${new Date()}.${format}`;
-    let blob = new Blob([content], { type: "text,plain;charset=utf-8" });
-    saveAs(blob, filename);
+    let filename = `jina-dashboard-debug-output-${new Date()}.${format}`
+    let blob = new Blob([content], { type: "text,plain;charset=utf-8" })
+    saveAs(blob, filename)
   },
-};
+}
 
-export default logger;
+export default logger
