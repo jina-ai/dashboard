516f26cfeb884161d5406ca56f4155529ce61763
BastinJafari <bastinjafari@gmail.com>
2021-03-03 18:45:38 +0100

    Refactor: type FormItem

--- src/components/Settings/FormItem.tsx
+++ src/components/Settings/FormItem.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import { Col, FormInput } from "shards-react";
-import { SettingName } from "../../redux/settings/settings.types";
+import React from "react"
+import { Col, FormInput } from "shards-react"
+import { SettingName } from "../../redux/settings/settings.types"
 
 type Props = {
-  colSpan?: number;
-  label: string;
-  value: SettingName;
-  placeholder: string;
-  onChange: (e: any) => void;
-};
+  colSpan?: number
+  label: string
+  value: SettingName
+  placeholder: string
+  onChange: (e: Event) => void
+}
 
 export default function FormItem({
   colSpan = 6,
@@ -22,5 +22,5 @@ export default function FormItem({
       <label>{label}</label>
       <FormInput placeholder={placeholder} value={value} onChange={onChange} />
     </Col>
-  );
+  )
 }
