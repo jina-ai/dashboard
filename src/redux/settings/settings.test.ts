ca5b9bc0fdd4cbd5a6653bbf0996b3faedd6e90d
BastinJafari <bastinjafari@gmail.com>
2021-03-04 18:59:03 +0100

    Refactor: type settings test

--- src/redux/settings/settings.test.ts
+++ src/redux/settings/settings.test.ts
@@ -1,47 +1,61 @@
-import reducer from "./settings.reducer";
-import { updateSettings } from "./settings.actions";
-import { testSettingsState, newTestSettings } from "./settings.testData";
+import reducer from "./settings.reducer"
+import { updateSettings } from "./settings.actions"
+import { testSettingsState, newTestSettings } from "./settings.testData"
+import { SettingName, Settings } from "./settings.types"
 
 describe("settings reducer", () => {
   it("should save settings in Store", () => {
     const newSettingsState = reducer(
       testSettingsState,
       updateSettings(newTestSettings)
-    );
-    expect(newSettingsState.settings).toEqual(newTestSettings);
-  });
+    )
+    expect(newSettingsState.settings).toEqual(newTestSettings)
+  })
 
   it("should save settings in localStorage", () => {
-    reducer(testSettingsState, updateSettings(newTestSettings));
-    const newSettingsAfterUpdate: any = {};
+    reducer(testSettingsState, updateSettings(newTestSettings))
+    const newSettingsAfterUpdate: Settings = {
+      log: "",
+      profile: "",
+      yaml: "",
+      host: "",
+      shutdown: "",
+      ready: "",
+      port: "",
+    }
     Object.keys(newTestSettings).forEach((key) => {
-      newSettingsAfterUpdate[key] = localStorage.getItem(`preferences-${key}`);
-    });
-    expect(newSettingsAfterUpdate).toEqual(newTestSettings);
-  });
+      const key2 = key as SettingName
+      newSettingsAfterUpdate[key2] = localStorage.getItem(
+        `preferences-${key}`
+      ) as string
+    })
+    expect(newSettingsAfterUpdate).toEqual(newTestSettings)
+  })
 
   it("should log the saved settings", () => {
     Object.defineProperty(window, "logsEnabled", {
       value: true,
       writable: true,
-    });
+    })
 
     Object.defineProperty(window, "logs", {
-      value: { push: (data: any) => {} },
+      value: {
+        push: () => {},
+      },
       writable: false,
-    });
+    })
 
-    console.log = jest.fn();
-    (window as any).logs.push = jest.fn();
+    console.log = jest.fn()
+    ;(window as any).logs.push = jest.fn()
 
-    reducer(testSettingsState, updateSettings(newTestSettings));
+    reducer(testSettingsState, updateSettings(newTestSettings))
 
     expect(console.log).toHaveBeenCalledWith("saveSettings - settings", {
       ...newTestSettings,
-    });
+    })
     expect((window as any).logs.push).toHaveBeenCalledWith([
       "saveSettings - settings",
       newTestSettings,
-    ]);
-  });
-});
+    ])
+  })
+})
