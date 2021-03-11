c565aa49d2e23e2f6d8ecb9f76d2be6ea8d84964
BastinJafari <bastinjafari@gmail.com>
2021-03-11 14:55:48 +0100

    Refactor: Login class to function

--- src/views/LogIn.tsx
+++ src/views/LogIn.tsx
@@ -1,42 +1,34 @@
 /* eslint jsx-a11y/anchor-is-valid: 0 */
 
-import React from "react";
-import { Container, Row, Col, Card, CardBody } from "shards-react";
+import React from "react"
+import { Container, Row, Col, Card, CardBody } from "shards-react"
 
-class Login extends React.Component {
-  componentDidMount = () => {
-    let hash = window.location.href;
-    const codeIndex = hash.indexOf("code");
-    if (codeIndex > 0) {
-    }
-  };
-  render = () => {
-    return (
-      <Container fluid className="main-content-container px-0">
-        <Row noGutters className="h-100">
-          <Col lg="3" md="5" className="auth-form mx-auto">
-            <Card>
-              <CardBody>
-                <img
-                  className="auth-form__logo d-table mx-auto mb-3"
-                  src="/jina-light.svg"
-                  style={{ maxWidth: "100px" }}
-                  alt="Jina"
-                />
-                <h5 className="auth-form__title text-center mb-4">Log in</h5>
-                <a
-                  id="github-button"
-                  className="btn btn-block btn-social btn-github"
-                >
-                  <i className="fab fa-github"></i> Sign in with GitHub
-                </a>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    );
-  };
+function Login() {
+  return (
+    <Container fluid className="main-content-container px-0">
+      <Row noGutters className="h-100">
+        <Col lg="3" md="5" className="auth-form mx-auto">
+          <Card>
+            <CardBody>
+              <img
+                className="auth-form__logo d-table mx-auto mb-3"
+                src="/jina-light.svg"
+                style={{ maxWidth: "100px" }}
+                alt="Jina"
+              />
+              <h5 className="auth-form__title text-center mb-4">Log in</h5>
+              <a
+                id="github-button"
+                className="btn btn-block btn-social btn-github"
+              >
+                <i className="fab fa-github"></i> Sign in with GitHub
+              </a>
+            </CardBody>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
-export default Login;
+export default Login
