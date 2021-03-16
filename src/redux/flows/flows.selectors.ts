7e84c6dd7c641dbdb699996823d476d83f4c3482
Sergiy Chemodanov <sergiychemodanov@gmail.com>
2021-03-15 22:27:53 -0500

    feat: workspace redux initial

--- src/redux/flows/flows.selectors.ts
+++ src/redux/flows/flows.selectors.ts
@@ -3,6 +3,9 @@ import { State } from "../index"
 export const selectSelectedFlow = (state: State) =>
   state.flowState.flows[state.flowState.selectedFlowId]
 
+export const selectSelectedWorkspace = (state: State) =>
+  state.flowState.workspaces[state.flowState.selectedWorkspaceId]
+
 export const selectTooltipConfig = (state: State) =>
   state.flowState.tooltipConfig
 
