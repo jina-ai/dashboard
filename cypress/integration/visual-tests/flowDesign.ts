00bdeab650fc2d675efcb6eed82d843ad2ab5f64
BastinJafari <bastinjafari@gmail.com>
2021-03-18 14:53:21 +0100

    refactor: Flowselection to Workspaceselection

--- cypress/integration/visual-tests/flowDesign.ts
+++ cypress/integration/visual-tests/flowDesign.ts
@@ -1,5 +1,3 @@
-import defaultPods from "../../../src/data/defaultPods"
-
 describe("Flow design workflow", () => {
   beforeEach(() => {
     cy.visit("/#/flow")
@@ -18,24 +16,28 @@ describe("Flow design workflow", () => {
     cy.get(".chart-section-container").trigger("drop", x, y, { dataTransfer })
   }
 
-  context("When a new flow is created", () => {
-    it("successfully let you pull new pods and connect them", () => {
-      let firstPortLabel = "gateway"
-      let secondPortLabel = defaultPods[1].name
-      defaultPods.forEach((pod, idx) => {
-        if (idx !== 0 && idx < defaultPods.length - 1) {
-          moveSideBarItemToCanvas(idx, 315, 100 + 50 * idx)
-          cy.dataName(`NodePortBottom-${firstPortLabel}`).trigger("mousedown", {
-            force: true,
-          })
-          cy.dataName(`NodePortTop-${secondPortLabel}`).trigger("mouseup", {
-            force: true,
-          })
-          firstPortLabel = secondPortLabel
-          secondPortLabel = defaultPods[idx + 1].name
-        }
-      })
+  type CartesianCoordinate = [number, number]
 
+  const connectPoints = (
+    startPoint: CartesianCoordinate,
+    endPoint: CartesianCoordinate
+  ) => {
+    cy.get(".chart-section-container").trigger("mousedown", ...startPoint, {
+      which: 1,
+    })
+    cy.get(".chart-section-container").trigger("mousemove", ...endPoint)
+    cy.get(".chart-section-container").trigger("mouseup", ...endPoint)
+  }
+
+  context("When a new flow is created", () => {
+    it("successfully let you pull new pods", () => {
+      moveSideBarItemToCanvas(1, 50, 100)
+      moveSideBarItemToCanvas(2, 50, 175)
+      connectPoints([180, 140], [180, 175])
+      moveSideBarItemToCanvas(3, 50, 250)
+      connectPoints([180, 215], [180, 250])
+      moveSideBarItemToCanvas(4, 50, 325)
+      connectPoints([180, 290], [180, 325])
       cy.percySnapshot("flow-design")
     })
   })
