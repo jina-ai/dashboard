b4d608007ff499f2332c49d1b411483504ebb727
BastinJafari <bastinjafari@gmail.com>
2021-03-23 12:14:05 +0100

    Feat: basic index and search

--- src/services/tests/gatewayClient.ts
+++ src/services/tests/gatewayClient.ts
@@ -5,7 +5,7 @@ import logger from "../../logger"
 import { TIMEOUT } from "../config"
 
 export let gatewayInstance = axios.create()
-
+//todo refactor messages into variables and share them with tests
 const gatewayClient = {
   connect: async (settings: Settings, callback: ConnectionCallback) => {
     logger.log("api - connect - settings", settings)
@@ -23,9 +23,37 @@ const gatewayClient = {
         })
       }
     } catch (e) {
-      logger.log("api - connect could not connect to jinad:", e)
+      logger.log("api - connect could not connect to gateway:", e)
     }
     return callback({ connected: false, message: "failed to connect" })
+  },
+  search: async (searchRequest: string) => {
+    try {
+      const result = await gatewayInstance.post("/search", {
+        top_k: 10,
+        mode: "search",
+        data: [searchRequest],
+      })
+      if (result.status === 200) {
+        logger.log("search - successful")
+      }
+    } catch (e) {
+      logger.log("search - error", e)
+    }
+  },
+  index: async (indexRequest: string) => {
+    try {
+      const result = await gatewayInstance.post("/index", {
+        top_k: 10,
+        mode: "search",
+        data: [indexRequest],
+      })
+      if (result.status === 200) {
+        logger.log("index - successful")
+      }
+    } catch (e) {
+      logger.log("index - error", e)
+    }
   },
 }
 
