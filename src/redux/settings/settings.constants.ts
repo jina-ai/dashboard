3695d50dc66f6659341e46df011070c122183002
BastinJafari <bastinjafari@gmail.com>
2021-03-24 09:36:42 +0100

    Fix: save settings to localStorage

--- src/redux/settings/settings.constants.ts
+++ src/redux/settings/settings.constants.ts
@@ -10,11 +10,13 @@ export const UPDATE_SETTINGS = "UPDATE_SETTINGS"
 export const intialSettings: SettingsState = {
   settings: {
     jinadHost:
-      localStorage.getItem("preferences-jinad-host") || defaultJinaDHost,
-    jinadPort: localStorage.getItem("preferences-port") || defaultJinaDPort,
+      localStorage.getItem("preferences-jinadhost") || defaultJinaDHost,
+    jinadPort:
+      localStorage.getItem("preferences-jinadPort") || defaultJinaDPort,
     gatewayHost:
-      localStorage.getItem("preferences-jinad-host") || defaultGatewayHost,
-    gatewayPort: localStorage.getItem("preferences-port") || defaultGatewayPort,
+      localStorage.getItem("preferences-gatewayHost") || defaultGatewayHost,
+    gatewayPort:
+      localStorage.getItem("preferences-gatewayPort") || defaultGatewayPort,
     log: localStorage.getItem("preferences-log") || "/stream/log",
     profile: localStorage.getItem("preferences-profile") || "/stream/profile",
     yaml: localStorage.getItem("preferences-yaml") || "/data/yaml",
