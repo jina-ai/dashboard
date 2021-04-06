17a479612062b9aeca54b8a7065a4095186046a1
BastinJafari <bastinjafari@gmail.com>
2021-04-06 14:16:27 +0200

    Fix: send list of files to one WS instead of creating many WS`

--- src/services/multiModalScript.ts
+++ src/services/multiModalScript.ts
@@ -2,9 +2,13 @@ import jinadClient from "./jinad"
 import { configData } from "../data/multiModalExample"
 
 export async function multiModalScript() {
+  const configFiles: (string | Blob)[] = []
+
   Object.entries(configData).forEach(([fileName, fileData]) => {
     const blob = new Blob([fileData], { type: "text/plain" })
     const file = new File([blob], fileName, { type: "text/plain" })
-    jinadClient.createWorkspace([file]).then((result) => console.log(result))
+    configFiles.push(file)
   })
+
+  jinadClient.createWorkspace(configFiles).then((result) => console.log(result))
 }
