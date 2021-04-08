bc36cfb1df2408ea84b481492e3c60b6388ad039
BastinJafari <bastinjafari@gmail.com>
2021-04-08 16:58:44 +0200

    Refactor: remove comments

--- src/services/multiModalScript.ts
+++ src/services/multiModalScript.ts
@@ -34,38 +34,7 @@ export async function multiModalScript() {
 
   logger.log(gatewayResult, "gatewayResult")
 
-  // const data = {
-  //   id: "26",
-  //   image: "image_26.jpg",
-  //   caption: "Attractive caucasian woman looking at camera.",
-  // }
-  //
-  // const data2 = {
-  //   id: indexData[0]["1"].toString(),
-  //   image: indexData[0]["image_1.jpg"],
-  //   caption:
-  //     indexData[0]["A beautiful young girl posing on a white background."],
-  // }
-  //
-  // const data3 = {
-  //   id: 2499,
-  //   image: "image_2499.jpg",
-  //   caption: "Italian Woman Holding Sliced Pizza To Eat in a Restaurant",
-  // }
-
-  // console.log(data)
-  // const gatewayClientResult = await gatewayClient.index(JSON.stringify(data))
-  // console.log(gatewayClientResult, "gatewayClientResult")
-  //
-  // console.log(data2)
-  // const gatewayClientResult2 = await gatewayClient.index(JSON.stringify(data2))
-  // console.log(gatewayClientResult2, "gatewayClientResult")
-
-  // console.log(data3)
-  // const gatewayClientResult3 = await gatewayClient.index(JSON.stringify(data3))
-  // console.log(gatewayClientResult3, "gatewayClientResult")
-
-  for (let i = 0; i < 100; i++) {
+  for (let i = 0; i < 10; i++) {
     console.log(i)
     const data = {
       id: indexData[i]["1"].toString(),
