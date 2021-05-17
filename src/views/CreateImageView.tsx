d70db5346b8e15f73dcdc59ced497ef8767f91f9
BastinJafari <bastinjafari@gmail.com>
2021-05-17 15:01:17 +0200

    initial commit

---
+++ src/views/CreateImageView.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+function CreateImageView() {
+  return <div>hallo ballo</div>
+}
+
+export default CreateImageView
--- src/views/HubView.tsx
+++ src/views/HubView.tsx
@@ -1,19 +1,20 @@
-import React, { useEffect } from "react";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
-import { Container, Row } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { fetchHubImages } from "../redux/hub/hub.actions";
-import { PageTitle } from "../components/Common/PageTitle";
-import HubOverviewActionsContainer from "../components/Hub/HubOverviewActionsContainer";
-import HubImagesListPreview from "../components/Hub/HubImagesListPreview";
-import HubImagesList from "../components/Hub/HubImagesList";
+import React, { useEffect } from "react"
+import { Switch, Route, useRouteMatch } from "react-router-dom"
+import { Container, Row } from "react-bootstrap"
+import { useDispatch } from "react-redux"
+import { fetchHubImages } from "../redux/hub/hub.actions"
+import { PageTitle } from "../components/Common/PageTitle"
+import HubOverviewActionsContainer from "../components/Hub/HubOverviewActionsContainer"
+import HubImagesListPreview from "../components/Hub/HubImagesListPreview"
+import HubImagesList from "../components/Hub/HubImagesList"
+import CreateImageView from "./CreateImageView"
 
 const HubView = () => {
-  const { path } = useRouteMatch();
-  const dispatch = useDispatch();
+  const { path } = useRouteMatch()
+  const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(fetchHubImages());
-  }, [dispatch]);
+    dispatch(fetchHubImages())
+  }, [dispatch])
 
   return (
     <Container fluid className="main-content-container px-0">
@@ -31,10 +32,13 @@ const HubView = () => {
           <Route path={`${path}/explore`}>
             <HubImagesList />
           </Route>
+          <Route path={`${path}/create`}>
+            <CreateImageView />
+          </Route>
         </Switch>
       </div>
     </Container>
-  );
-};
+  )
+}
 
-export default HubView;
+export default HubView
