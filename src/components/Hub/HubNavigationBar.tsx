70c2da27251d56e45d541e8135f344f31d29504b
BastinJafari <bastinjafari@gmail.com>
2021-05-20 17:25:30 +0200

    initial commit

---
+++ src/components/Hub/HubNavigationBar.tsx
@@ -0,0 +1,31 @@
+import React from "react"
+import { AppBar, Tab, Tabs } from "@material-ui/core"
+
+function a11yProps(index: any) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  }
+}
+
+export default function HubNavigationBar() {
+  const [value, setValue] = React.useState(0)
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
+    setValue(newValue)
+  }
+
+  return (
+    <AppBar position={"static"}>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        aria-label="simple tabs example"
+      >
+        <Tab label="Item One" {...a11yProps(0)} />
+        <Tab label="Item Two" {...a11yProps(1)} />
+        <Tab label="Item Three" {...a11yProps(2)} />
+      </Tabs>
+    </AppBar>
+  )
+}
--- src/views/HubView.tsx
+++ src/views/HubView.tsx
@@ -1,22 +1,24 @@
-import React, { useEffect } from "react";
-import { Switch, Route, useRouteMatch } from "react-router-dom";
-import { Container, Row } from "react-bootstrap";
-import { useDispatch } from "react-redux";
-import { fetchHubImages } from "../redux/hub/hub.actions";
-import { PageTitle } from "../components/Common/PageTitle";
-import HubOverviewActionsContainer from "../components/Hub/HubOverviewActionsContainer";
-import HubImagesListPreview from "../components/Hub/HubImagesListPreview";
-import HubImagesList from "../components/Hub/HubImagesList";
+import React, { useEffect } from "react"
+import { Switch, Route, useRouteMatch } from "react-router-dom"
+import { Container, Row } from "react-bootstrap"
+import { useDispatch } from "react-redux"
+import { fetchHubImages } from "../redux/hub/hub.actions"
+import { PageTitle } from "../components/Common/PageTitle"
+import HubOverviewActionsContainer from "../components/Hub/HubOverviewActionsContainer"
+import HubImagesListPreview from "../components/Hub/HubImagesListPreview"
+import HubImagesList from "../components/Hub/HubImagesList"
+import HubNavigationBar from "../components/Hub/HubNavigationBar"
 
 const HubView = () => {
-  const { path } = useRouteMatch();
-  const dispatch = useDispatch();
+  const { path } = useRouteMatch()
+  const dispatch = useDispatch()
   useEffect(() => {
-    dispatch(fetchHubImages());
-  }, [dispatch]);
+    dispatch(fetchHubImages())
+  }, [dispatch])
 
   return (
     <Container fluid className="main-content-container px-0">
+      <HubNavigationBar />
       <div className="px-4">
         <Row className="page-header">
           <PageTitle title="Jina Hub" className="text-sm-left mb-3" />
@@ -34,7 +36,7 @@ const HubView = () => {
         </Switch>
       </div>
     </Container>
-  );
-};
+  )
+}
 
-export default HubView;
+export default HubView
