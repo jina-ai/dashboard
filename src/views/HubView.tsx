634d82d9d2f3c653d9b93bfbe4fac4dbe7ab0c71
BastinJafari <bastinjafari@gmail.com>
2021-05-25 10:13:51 +0100

    Refactor: Remove NavBar

--- src/views/HubView.tsx
+++ src/views/HubView.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect } from "react"
 import { Switch, Route, useRouteMatch } from "react-router-dom"
 import { Container, Row } from "react-bootstrap"
 import { useDispatch } from "react-redux"
@@ -7,7 +7,6 @@ import { PageTitle } from "../components/Common/PageTitle"
 import HubOverviewActionsContainer from "../components/Hub/HubOverviewActionsContainer"
 import HubImagesListPreview from "../components/Hub/HubImagesListPreview"
 import HubImagesList from "../components/Hub/HubImagesList"
-import HubNavigationBar from "../components/Hub/HubNavigationBar"
 
 const HubView = () => {
   const { path } = useRouteMatch()
@@ -16,22 +15,8 @@ const HubView = () => {
     dispatch(fetchHubImages())
   }, [dispatch])
 
-  const [tabNumber, setTabNumber] = useState(0)
-
-  const handleChange = (event: React.ChangeEvent<{}>, newValue: number) => {
-    setTabNumber(newValue)
-  }
-
-  const handleSearch = (searchString: string) => {
-    alert(searchString)
-  }
   return (
     <Container fluid className="main-content-container px-0">
-      <HubNavigationBar
-        handleSearch={handleSearch}
-        handleChange={handleChange}
-        tabNumber={tabNumber}
-      />
       <div className="px-4">
         <Row className="page-header">
           <PageTitle title="Jina Hub" className="text-sm-left mb-3" />
