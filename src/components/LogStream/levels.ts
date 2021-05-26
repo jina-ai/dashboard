881ddfb04a01d962a5ecb789858755cd8d623d5f
BastinJafari <bastinjafari@gmail.com>
2021-05-26 22:43:00 +0100

    fix: palette bugs

--- src/components/LogStream/levels.ts
+++ src/components/LogStream/levels.ts
@@ -14,27 +14,27 @@ export const getLevelPalette = (theme: Theme): LevelColors => {
   const colorPalette = theme.palette
   return {
     INFO: {
-      borderColor: colorPalette.info,
+      borderColor: colorPalette.info.main,
       backgroundColor: "rgba(0, 153, 153, 0.9)",
     },
     SUCCESS: {
-      borderColor: colorPalette.success,
+      borderColor: colorPalette.success.main,
       backgroundColor: "rgba(50, 200, 205, 0.9)",
     },
     WARNING: {
-      borderColor: colorPalette.warning,
+      borderColor: colorPalette.warning.main,
       backgroundColor: "rgba(255, 204, 102, 0.9)",
     },
     ERROR: {
-      borderColor: colorPalette.error,
+      borderColor: colorPalette.error.main,
       backgroundColor: "rgba(255, 102, 102, 0.9)",
     },
     CRITICAL: {
-      borderColor: colorPalette.critical,
+      borderColor: colorPalette.warning.dark,
       backgroundColor: "rgba(255, 70, 64, 0.9)",
     },
     DEBUG: {
-      borderColor: colorPalette.debug,
+      borderColor: colorPalette.grey[700],
       backgroundColor: "rgba(110, 114, 120, 0.9)",
     },
   }
