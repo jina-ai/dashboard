832aeb15171d71b2e53c77d18b98caf602110917
Roshan Jossy <roshan.jossy@jina.ai>
2021-06-04 13:27:03 +0200

    refactor: RoutingTable to seperate component.
     - Add elapsed time for routes

--- src/views/DebuggingTool.tsx
+++ src/views/DebuggingTool.tsx
@@ -5,12 +5,8 @@ import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
 import Grid from "@material-ui/core/Grid"
 import Box from "@material-ui/core/Box"
-import Table from "@material-ui/core/Table"
-import TableBody from "@material-ui/core/TableBody"
-import TableCell from "@material-ui/core/TableCell"
-import TableHead from "@material-ui/core/TableHead"
-import TableRow from "@material-ui/core/TableRow"
 import axios from "axios"
+import RoutesTable from "../components/DebuggingTool/RouteTable"
 
 const TextInput = styled(TextField)`
   width: 100%;
@@ -28,23 +24,23 @@ const ResponseTitle = styled.h6`
   font-size: 1rem;
   color: ${(props) => props.theme.palette.grey[700]};
 `
-type Route = {
-  pod: string
-  pod_id: string
-  start_time: string
-  end_time?: string
-}
+const reqBody = `{ "data": [ {"text": "hello world"}] }`
 
 const DebuggingTool = () => {
   const [response, setResponse] = useState([])
-  const [host, setHost] = useState("")
-  const [port, setPort] = useState("")
+  const [host, setHost] = useState("127.0.0.1")
+  const [port, setPort] = useState("45678")
   const [requestBody, setRequestBody] = useState("")
   const handleRequest = async () => {
-    const searchResult = await axios.post(
-      `http://${host}:${port}/search`,
-      requestBody
-    )
+    console.log(`http://${host}:${port}/search`)
+    const searchResult = await axios({
+      method: 'post',
+      url: `http://${host}:${port}/search`,
+      data: reqBody,
+      headers: {
+        mode: 'no-cors'
+      }
+    })
     if (searchResult) {
       setResponse(searchResult.data)
     }
@@ -56,6 +52,7 @@ const DebuggingTool = () => {
           <TextInput
             label="Host"
             variant="outlined"
+            value="127.0.0.1"
             onChange={(e) => setHost(e.target.value)}
           />
         </Grid>
@@ -63,6 +60,7 @@ const DebuggingTool = () => {
           <TextInput
             label="Port"
             variant="outlined"
+            value="45678"
             onChange={(e) => setPort(e.target.value)}
           />
         </Grid>
@@ -79,6 +77,7 @@ const DebuggingTool = () => {
             multiline
             minRows={4}
             variant="outlined"
+            value={reqBody}
             onChange={(e) => setRequestBody(e.target.value)}
           />
         </Grid>
@@ -89,8 +88,8 @@ const DebuggingTool = () => {
               {response && response?.routes?.length > 0 ? (
                 <RoutesTable routes={response.routes} />
               ) : (
-                <></>
-              )}
+                  <></>
+                )}
             </ResponseContainer>
           </Box>
         </Grid>
@@ -98,7 +97,7 @@ const DebuggingTool = () => {
           <Box>
             <ResponseContainer>
               <ResponseTitle>Documents and matches</ResponseTitle>
-              {response && response.length > 0 ? <>{}</> : <></>}
+              {response && response.length > 0 ? <>{ }</> : <></>}
             </ResponseContainer>
           </Box>
         </Grid>
@@ -106,34 +105,4 @@ const DebuggingTool = () => {
     </Container>
   )
 }
-
-type RoutesTableProps = {
-  routes: Route[]
-}
-
-const RoutesTable = ({ routes }: RoutesTableProps) => {
-  return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell>Pod</TableCell>
-          <TableCell>Pod Id</TableCell>
-          <TableCell>Start time</TableCell>
-          <TableCell>End time</TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {routes.map((route) => (
-          <TableRow key={route.pod_id}>
-            <TableCell> {route.pod} </TableCell>
-            <TableCell>{route.pod_id}</TableCell>
-            <TableCell>{route.start_time}</TableCell>
-            <TableCell>{route.end_time}</TableCell>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
-  )
-}
-
 export default DebuggingTool
