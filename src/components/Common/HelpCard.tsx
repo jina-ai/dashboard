40d04d4762997b41d3cfa9459c9d97cccc0709a7
Roshan Jossy <roshan.jossy@jina.ai>
2021-08-30 12:43:15 +0200

    style: help view cards

--- src/components/Common/HelpCard.tsx
+++ src/components/Common/HelpCard.tsx
@@ -1,13 +1,14 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode } from "react"
+import Card from "./Card"
 
 type Props = {
-  title: ReactNode;
-  content: string;
-  icon: string;
-  theme: string;
-  link: string;
-  dataName: string;
-};
+  title: ReactNode
+  content: string
+  icon: string
+  theme: string
+  link: string
+  dataName: string
+}
 
 function HelpCard({ title, content, icon, theme, link, dataName }: Props) {
   return (
@@ -18,25 +19,21 @@ function HelpCard({ title, content, icon, theme, link, dataName }: Props) {
       rel="noopener noreferrer"
       data-name={dataName}
     >
-      <div className="h-100">
-        <div className="pt-3">
-          <div className="align-items-center">
+      <Card className="h-full">
+        <div className=" flex flex-col pt-3">
+          <div className="flex flex-row justify-between mb-8">
             <div>
-              <h4>{title}</h4>
+              <h4 className="text-2xl text-gray-700">{title}</h4>
             </div>
-            <div>
-              <h1 className="float-right">
-                <span className={`${icon} log-${theme}`} />
-              </h1>
-            </div>
+            <div className={`${icon} log-${theme} text-5xl`} />
           </div>
           <div>
-            <div>{content}</div>
+            <div className="text-gray-500">{content}</div>
           </div>
         </div>
-      </div>
+      </Card>
     </a>
-  );
+  )
 }
 
-export { HelpCard };
+export { HelpCard }
--- src/views/NotFound.tsx
+++ src/views/NotFound.tsx
@@ -1,69 +1,59 @@
-import React from "react";
-import { HelpCard } from "../components/Common/HelpCard";
-import { PageTitle } from "../components/Common/PageTitle";
+import React from "react"
+import { HelpCard } from "../components/Common/HelpCard"
+import { PageTitle } from "../components/Common/PageTitle"
 
 const NotFound = () => (
   <div className="main-content-container px-4" data-name="helpPage">
     <div className="page-header mb-4">
       <PageTitle title="Resources" className="text-sm-left mb-3" />
     </div>
-    <div>
-      <div className="mb-4">
-        <HelpCard
-          title="Jina 101"
-          content="Understanding all key concepts of Jina in 3 minutes!"
-          link="https://101.jina.ai"
-          icon="fas fa-egg"
-          theme="warning"
-          dataName="jina-101-card"
-        />
-      </div>
-      <div className="mb-4">
-        <HelpCard
-          title="Jina Docs"
-          content="The best way to learn Jina in depth."
-          link="https://docs.jina.ai"
-          icon="fas fa-book"
-          theme="primary"
-          dataName="jina-docs-card"
-        />
-      </div>
-      <div className="mb-4">
-        <HelpCard
-          title='Jina "Hello World"'
-          content="A simple demo of image neural search for Fashion-MNIST."
-          link="https://github.com/jina-ai/jina#jina-hello-world-"
-          icon="fas fa-globe-europe"
-          theme="error"
-          dataName="jina-hello-world-card"
-        />
-      </div>
-      <div className="mb-4">
-        <HelpCard
-          title={
-            <span>
-              Visit us on <span className="text-nowrap">Github!</span>
-            </span>
-          }
-          content="Explore more exciting open-source projects from us."
-          link="https://opensource.jina.ai"
-          icon="fab fa-github"
-          theme="info"
-          dataName="jina-github-card"
-        />
-      </div>
-      <div className="mb-4">
-        <HelpCard
-          title="We are hiring!"
-          content="Join us and build the next neural search eco system!"
-          link="https://www.linkedin.com/company/jinaai/jobs/"
-          icon="fab fa-linkedin"
-          theme="linkedin"
-          dataName="jina-linkedin-card"
-        />
-      </div>
+    <div className="grid grid-cols-4 gap-4">
+      <HelpCard
+        title="Jina 101"
+        content="Understanding all key concepts of Jina in 3 minutes!"
+        link="https://101.jina.ai"
+        icon="fas fa-egg"
+        theme="warning"
+        dataName="jina-101-card"
+      />
+      <HelpCard
+        title="Jina Docs"
+        content="The best way to learn Jina in depth."
+        link="https://docs.jina.ai"
+        icon="fas fa-book"
+        theme="primary"
+        dataName="jina-docs-card"
+      />
+      <HelpCard
+        title='Jina "Hello World"'
+        content="A simple demo of image neural search for Fashion-MNIST."
+        link="https://github.com/jina-ai/jina#jina-hello-world-"
+        icon="fas fa-globe-europe"
+        theme="error"
+        dataName="jina-hello-world-card"
+      />
+      <HelpCard
+        title={
+          <span>
+            Visit us on <span className="text-nowrap">Github!</span>
+          </span>
+        }
+        content="Explore more exciting open-source projects from us."
+        link="https://opensource.jina.ai"
+        icon="fab fa-github"
+        theme="info"
+        dataName="jina-github-card"
+      />
+      <HelpCard
+        title="We are hiring!"
+        content="Join us and build the next neural search eco system!"
+        link="https://www.linkedin.com/company/jinaai/jobs/"
+        icon="fab fa-linkedin"
+        theme="linkedin"
+        dataName="jina-linkedin-card"
+      />
     </div>
   </div>
-);
+)
 
-export default NotFound;
+export default NotFound
