90f5b5e03a6e0bed2adc2b6e2e78612642650f1d
Saurav Gupta <saurav.-@uni-ulm.de>
2021-09-15 01:11:56 +0200

    fix(LogsTableHeader): fix header style of LogsTableHeader

--- src/components/LogStream/LogsTableHeader.tsx
+++ src/components/LogStream/LogsTableHeader.tsx
@@ -20,20 +20,20 @@ const LogsTableHeader = ({
     >
       <div className="flex">
         <div
-          className="log-prefix text-muted px-0 flex flex-row"
+          className="log-prefix text-muted px-0 flex md:flex-1"
           css={{ maxWidth: firstCol }}
         >
-          <div className="text-bold mr-2">#</div>
-          <div className="ml-auto">timestamp</div>
+          <div className="text-bold mr-2 text-lightGray">#</div>
+          <div className="ml-auto text-lightGray">timestamp</div>
         </div>
         <div
-          className="log-prefix px-0 text-left text-md-right text-bold"
+          className="log-prefix px-0 text-right text-md-right text-bold md:flex-1"
           css={{ maxWidth: secondCol }}
         >
           source@process[level]:
         </div>
         <div
-          className="px-0"
+          className="px-0 md:flex-1 text-lightGray"
           css={{
             maxHeight: 84,
             display: "block",
